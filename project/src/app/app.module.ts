d563effad5750bf94b74bfa39549a2e1c5a4a39e
Cristian C. Gorrin <cristian_gorrin@live.com>
2017-08-26 12:17:34 +0200

    Init homepage component

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PageHomepageComponent } from './page-homepage/page-homepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageHomepageComponent
   ],
   imports: [
     BrowserModule
---
+++ project/src/app/page-homepage/page-homepage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page-homepage',
+  templateUrl: './page-homepage.component.html',
+  styleUrls: ['./page-homepage.component.sass']
+})
+export class PageHomepageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
