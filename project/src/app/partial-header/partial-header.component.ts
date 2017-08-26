498ff1882a5fbde32a35d843e2d01330e61dde1b
Cristian C. Gorrin <cristian_gorrin@live.com>
2017-08-26 18:03:30 +0200

    The default header component

--- project/src/app/partial-header/partial-header.component.ts
+++ project/src/app/partial-header/partial-header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-partial-header',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./partial-header.component.sass']
 })
 export class PartialHeaderComponent implements OnInit {
+  opt_template: string = '';
+  templates_name = {
+    homepage: '/homepage',
+    collection_page: '/collection_page'
+  }
 
-  constructor() { }
+  constructor(private route:Router) {
+    route.events.subscribe((val) => {
+      if (val instanceof NavigationEnd) {
+        this.opt_template = route.url.split('?')[0];
+      }
+  });
+  }
 
   ngOnInit() {
   }
-
 }
