8c8c6a972b6f2aad929145c03035286d7524739a
Cristian C. Gorrin <cristian_gorrin@live.com>
2017-08-30 21:36:10 +0200

    Complete the footer

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 
+// Component import
 import { AppComponent } from './app.component';
 import { PageHomepageComponent } from './page-homepage/page-homepage.component';
 import { PartialHeaderComponent } from './partial-header/partial-header.component';
